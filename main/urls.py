c49149376e04611222b9e3876445005ac345530d
acer <acer@someplace.com>
2021-12-22 13:44:45 +0700

    Project x

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('turunan/', views.turunan, name='turunan'),
+    path('integral/', views.integral, name='integral'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,41 @@
+import contextlib
+
 from django.shortcuts import render
+from sympy import *
 
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def turunan(request):
+    if request.method == 'POST':
+        var1 = request.POST['var1']
+        x = Symbol(var1)
+        y = request.POST['pers']
+        yprime = diff(y,x)
+
+        context = {
+            'yprime': yprime,
+            'y': y,
+            'var1' : var1
+        }
+        return render(request, 'main/turunan.html', context)
+    else:
+        return render(request, 'main/turunan.html')
+
+def integral(request):
+    if request.method == 'POST':
+        var1 = request.POST['var1']
+        x = Symbol(var1)
+        y = request.POST['pers']
+        yprime = integrate(y,x)
+
+        context = {
+            'yprime': yprime,
+            'y': y,
+            'var1' : var1
+        }
+        return render(request, 'main/integral.html', context)
+    else:
+        return render(request, 'main/integral.html')
