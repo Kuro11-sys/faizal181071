fbcc9c197af16460929fc385d7f17738f9cf9e39
acer <acer@someplace.com>
2021-12-22 14:19:59 +0700

    Project x

--- odesol/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OdesolConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'odesol'
--- odesoll/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OdesollConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'odesoll'
